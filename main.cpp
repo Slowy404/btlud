80331707e99dd600ce4ae9976f4f282fdb279be1
Slowy404 <111565576+Slowy404@users.noreply.github.com>
2023-02-22 09:01:45 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,28 @@ void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int
         << ", phoenixdown=" << phoenixdown
         << ", rescue=" << rescue << endl;
 }
+int readNumber(int map[],string len){
+    int j = 1;
+    int start = 0;
+    int end = len.find(" ");
+    while (end != string::npos) {
+        string str_num = len.substr(start, end - start);
+        map[j] = stoi(str_num);
+        j++;
+        start = end + 1;
+        end = len.find(" ", start);
+    }
+    string str_num = len.substr(start, end);
+    map[j] = stoi(str_num);
+    j++;
+    return j;
+}
+/*hàm trong while*/
+void checkLevel(int &level){
+  if(level>10) level=10;
+}
+
+/*mã sự kiện từ 1 đến 5*/
 int lvO(int local){
   int b = local%10;
   int level = local > 6 ? (b > 5 ? b : 5) : b;
@@ -22,9 +44,6 @@ int sl(int levelO,int level){
     return -1;
   }
   return 0;
-}
-void checkLevel(int &level){
-  if(level>10) level=10;
 }
 int damage1to5(int HP,int levelO,int &phoenixdown,int MaxHP,int codeEvent, int &rescue){
   double baseDamage ;
@@ -61,13 +80,13 @@ void checkHP(int HP,int &rescue,int &phoenixdown,int &check,int MaxHP,int &count
   if(HP<0 && phoenixdown < 1) rescue =0;
   if(HP<0 && phoenixdown > 1) {
     phoenixdown--;
-    check=0;
     HP=MaxHP;
     counter=0;
+    check=0;
   }
 }
-int lowpeopleHP(int HP,int MaxHP,int check){
-  if(check==1){
+/*Sự kiện 6*/
+int lowpeopleHP(int HP){
     if(HP < 5){
       HP=1;
       return HP;
@@ -75,84 +94,169 @@ int lowpeopleHP(int HP,int MaxHP,int check){
       HP=HP/5;
       return HP;
     }
-  } else{
-    HP=HP*5;
-    if(HP>MaxHP){
-      HP=MaxHP;
-    }
-    return HP;
-  } 
 }
+void cure(int &check_people,int &check_frog, int &maidenkiss, int &remedy, int &regLevel,int &counter,int &counterf, int &HP ,int &level){
+    //thuốc giải
+    if(check_people==1 && remedy > 0){
+        check_people = 0;
+        remedy--;
+        counter=0;
+        HP=HP*5;
+    } else ;
+    if(check_frog==1 && maidenkiss > 0){
+        check_frog = 0;
+        maidenkiss--;
+        counterf=0;
+        level=regLevel;
+    }
+}
+// sự kiện 11
+bool prime(int n) 
+{ 
+    if(n<=1) return 0;
+    for(int i=2;i<n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+} 
+int prePrime(int HP)
+{ 
+    int prePrime = HP; 
+    bool found = false; 
+    if (HP <= 1) 
+        return 2; 
+    while (!found) 
+    { 
+        prePrime++; 
+        if (prime(prePrime)) found = true; 
+    } 
+    return prePrime; 
+} 
 int MushMario(int level,int phoenixdown, int HP, int MaxHP){
   int n1=((level+phoenixdown)%5+1)*3;
-  int s1;
+  int s1=0;
   for(int i=0,j=99;i<n1;i++){
+    cout<<j<<"\t";
     s1=s1+j;
     j-=2;
   }
   HP=HP+(s1%100);
+  HP=prePrime(HP);
   if(HP>MaxHP){
     return MaxHP;
   }
   return HP;
 }
+// sự kiện 12
 int  Fibonacci_MushFibo(int HP){
      if (HP == 1) {
         return 1;
     } else {
-        int previous = 0;
-        int current = 1;
+        int pre = 0;
+        int cur = 1;
         int next = 1;
-        while (next <= HP) {
-            previous = current;
-            current = next;
-            next = previous + current;
+        while (next < HP) {// coi lại 
+            pre = cur;
+            cur = next;
+            next = pre + cur;
         }
-        return current;
+        return cur;
     }
 }
 void clam(int &item){
   item++;
   if(item>99) item=99;
 }
-bool prime(int HP){
-  for(int i=2;i<HP;i++){
-    if(HP%i==0) return 0;
-  }
-  return 1;
-}
-int readNumber(int map[],string len){
-    int j = 1;
-    int start = 0;
-    int end = len.find(" ");
-    while (end != string::npos) {
-        string str_num = len.substr(start, end - start);
-        map[j] = stoi(str_num);
-        j++;
-        start = end + 1;
-        end = len.find(" ", start);
-    }
-    string str_num = len.substr(start, end);
-    map[j] = stoi(str_num);
-    j++;
-    return j;
-}
+// sự kiện 13 ms
 void MushGhost(string mush_ghost, int line[], int &n2){
-  ifstream inMush(mush_ghost);
-  if(!inMush.is_open()){
-  } else {
-  int i=0;
-  inMush>>n2;
-  inMush.ignore();
-  string line2;
-  while(!inMush.eof()){
-    getline(inMush,line2,',');
-    // cout<<line2<<" ";
-    line[i]=stoi(line2);
-    i++;
-  }}
-  // cout<<endl;
-  inMush.close();
+    ifstream inMush(mush_ghost);
+    if(!inMush.is_open()){
+    } else {
+    int i=0;
+    inMush>>n2;
+    inMush.ignore();
+    string line2;
+    while(!inMush.eof()){
+        getline(inMush,line2,',');
+        // cout<<line2<<" ";
+        line[i]=stoi(line2);
+        i++;
+    }}
+    // cout<<endl;
+    inMush.close();
+}
+int checkMountain(int len[],int n2,int &mtx,int &mti) {
+    int max=len[0],j=0;
+    for(int i=0;i<n2;i++){
+        if(len[i]>max) {
+        max=len[i];
+        j=i;
+        }
+    }
+    for(int i=0;i<j-1;i++){
+        if(len[i]>len[i+1]) {
+        mtx = -2; 
+        mti = -3;
+        return 0;
+        }
+    }
+    for(int i=j;i<n2-1;i++){
+        if(len[i]<len[i+1]) {
+        mtx = -2; 
+        mti = -3;
+        return 0;
+        }
+    }
+    mtx=max;
+    mti=j;
+    return 1;
+}
+int checkMush3_4(int len[],int n2,int &maxi2,int &mini2,int &max2_3x,int &max2_3i){
+    for (int i=0;i<n2;i++){
+        if(len[i]<0){
+        len[i]=-len[i];
+        } else {
+        len[i]=(17*len[i]+9)%257;
+        }
+    }
+    int max=len[0];
+    int min=len[0];
+    for (int i=0;i<n2;i++){
+        if(len[i]>max){
+        max=len[i];
+        }
+        if(len[i]<min){
+        min=len[i];
+        }
+    }
+    for(int i=0;i<n2;i++){
+        if(max==len[i]){
+        maxi2=i;
+        break;
+        }
+    }
+    for(int i=0;i<n2;i++){
+        if(min==len[i]){
+        mini2=i;
+        break;
+        }
+    }
+    int m=min;
+    for(int i=0;i<n2;i++){
+        if(len[i]>m && len[i]!=max){
+        m=len[i];
+        }
+    }
+    for(int i=0;i<3;i++){
+        if(len[i]==m){
+        max2_3x=m;
+        max2_3i=i;
+        return 0;
+        }
+    }
+    max2_3x=-5;
+    max2_3i=-7;
+    return 0;
 }
 void Asclepius(string asclepius_pack, int r1,int c1,int &remedy, int &maidenkiss,int &phoenixdown){
   int M[10000];
@@ -188,7 +292,7 @@ void Asclepius(string asclepius_pack, int r1,int c1,int &remedy, int &maidenkiss
 void merlin(string merlin_pack,int &HP,int MaxHP){
   ifstream inMerlin(merlin_pack);
    if(!inMerlin.is_open()){
-  } else {
+    } else {
     int n9;
     inMerlin>>n9;
     inMerlin.ignore();
@@ -207,85 +311,14 @@ void merlin(string merlin_pack,int &HP,int MaxHP){
         }
       }
       if(isItem==1){
-        if(item_name.find("merlin")!=string::npos){
+        if(item_name.find("merlin")==string::npos){
           HP=HP+2;
         } else HP=HP+3;
         if(HP>MaxHP) HP=MaxHP;
       }
     }
   }
-}
-int checkMountain(int len[],int n2,int &mtx,int &mti) {
-  int max=len[0],j=0;
-  for(int i=0;i<n2;i++){
-    if(len[i]>max) {
-      max=len[i];
-      j=i;
-    }
-  }
-  for(int i=0;i<j;i++){
-    if(len[i]>len[i+1]) {
-      mtx = -2; 
-      mti = -3;
-      return 0;
-    }
-  }
-  for(int i=j;i<n2;i++){
-    if(len[i]<len[i+1]) {
-      mtx = -2; 
-      mti = -3;
-      return 0;
-    }
-  }
-  mtx=max;
-  mti=j;// bắt đầu từ 0
-  return 1;
-}
-int checkMush3_4(int len[],int n2,int &maxi2,int &mini2,int &max2_3x,int &max2_3i){
-  for (int i=0;i<n2;i++){
-    if(len[i]<0){
-      len[i]=-len[i];
-    } else {
-      len[i]=(17*len[i]+9)%257;
-    }
-  }
-  int max=len[0];
-  int min=len[0];
-  for (int i=0;i<n2;i++){
-    if(len[i]>max){
-      max=len[i];
-    }
-    if(len[i]<min){
-      min=len[i];
-    }
-  }
-  for(int i=0;i<n2;i++){
-    if(max==len[i]){
-      maxi2=i;
-      break;
-    }
-  }
-  for(int i=0;i<n2;i++){
-    if(min==len[i]){
-      mini2=i;
-      break;
-    }
-  }
-  int m=min;
-  for(int i=0;i<n2;i++){
-    if(len[i]>m && len[i]!=max){
-      m=len[i];
-    }
-  }
-  for(int i=0;i<3;i++){
-    if(len[i]==m){
-      max2_3x=m;
-      max2_3i=i;
-      return 0;
-    }
-  }
-  max2_3x=-5;
-  max2_3i=-7;
+  inMerlin.close();
 }
 void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue) 
 {
@@ -315,6 +348,8 @@ void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, in
     string mush_ghost;
     string merlin_pack;
     string asclepius_pack;
+    int large;
+    int small;
     int Merlin=0;
     int j=1;
     int asclepius=0;
@@ -405,7 +440,7 @@ void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, in
         check_people=1;
         counter=4;
         int HP_pre=HP;
-        HP=lowpeopleHP(HP,MaxHP,check_people);
+        HP=lowpeopleHP(HP);
         if(remedy >= 1){
           check_people = 0;
           remedy--;
@@ -502,10 +537,17 @@ void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, in
           {
             case '1':
               MushGhost(mush_ghost,len,n2);
-              maxi=len[0];
-              mini=len[0];
-              for (int j=0;j<n2;j++){
-                if(len[j]>maxi) maxi=len[j]; else if(len[j]<mini) mini=len[j];
+              large=len[0];
+              small=len[0];
+              for (int l=0;l<n2;l++){
+                if(len[l]<=small) {
+                  small=len[l];
+                  mini=l;
+                  } else ;
+                  if(len[l]>=large){
+                 large=len[l];
+                  maxi=l;
+                } else ;
               }
               HP=HP-(maxi+mini);
               break;
